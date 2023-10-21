bd4825dd29a1cfd0c3f9cea223ead8b9544f6470
Anirudh <50363140+AnirudhPotturi@users.noreply.github.com>
2023-10-21 12:53:54 -0700

    feat: adding input loop

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import DownloadRunner as dr
 
-print("Enter quality of video \n1. Standard (max 720p)\n2. Best available resolution\n3. Audio only")
+print("Enter quality of video \n1. Standard (max 720p)\n2. Best available resolution\n3. Audio only\nq - Quit")
 
 quality = input()
 
-print("Paste the link of your video:\t")
+while(quality != 'q'):
+    #
+    print("Paste the link of your video:\t")
 
-youtubeLink = input()
+    youtubeLink = input()
 
-dr.runner(youtubeLink, quality)
+    dr.runner(youtubeLink, quality)
 
-if int(quality) <= 2 :
-    print('Your video has been downloaded!')
-else:
-    print('Your audio has been downloaded!')
+    if int(quality) <= 2 :
+        print('Your video has been downloaded!')
+    else:
+        print('Your audio has been downloaded!')
+
+    print("Enter quality of video \n1. Standard (max 720p)\n2. Best available resolution\n3. Audio only\nq - Quit")
+
+    quality = input()
